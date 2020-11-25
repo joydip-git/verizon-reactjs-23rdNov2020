9db704602eb9574d7857b5750dcb5347066c8e3c
siemens-c2c <joy_mon78@yahoo.co.in>
2020-11-25 10:38:16 +0530

    react app update

---
+++ codes/day-3/react-concepts-demo/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+/**
+ * classes = {App-Header, App:}
+ */
+function App() {
+  return (
+    <div className="App">
+      Hello React JS
+    </div>
+  );
+}
+
+export default App;
