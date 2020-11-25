39fc1b825bf898a476fc17d5b8edc64ecbc64a2c
siemens-c2c <joy_mon78@yahoo.co.in>
2020-11-25 12:50:12 +0530

    person details updated

--- codes/day-3/react-concepts-demo/src/components/PersonDetail.js
+++ codes/day-3/react-concepts-demo/src/components/PersonDetail.js
@@ -3,21 +3,38 @@ import React from 'react'
 function PersonDetail(props) {
     const { personData } = props;
     const inputBackColor = {
-        backgroundColor: 'burlywood'
+        backgroundColor: 'burlywood',
+        color: 'black'
     }
     return (
         <table style={{ margin: '10px' }}>
             <tr>
                 <td>Name:&nbsp;</td>
-                <td><input type='text' value={personData.name} style={inputBackColor} /></td>
+                <td>
+                    <input
+                        type='text'
+                        value={personData.name}
+                        style={inputBackColor}
+                        onChange={ } /></td>
             </tr>
             <tr>
                 <td>Age:&nbsp;</td>
-                <td><input type='text' value={personData.age} style={inputBackColor} /></td>
+                <td>
+                    <input
+                        type='text'
+                        value={personData.age}
+                        style={inputBackColor}
+                        onChange={ } />
+                </td>
             </tr>
             <tr>
                 <td>Location:&nbsp;</td>
-                <td><input type='text' value={personData.location} style={inputBackColor} /></td>
+                <td><input
+                    type='text'
+                    value={personData.location}
+                    style={inputBackColor}
+                    onChange={ } />
+                </td>
             </tr>
         </table>
     )
